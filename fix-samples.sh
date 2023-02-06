7579adf1cd80bf04da2d4946bf6a45bcb0f3a755
frannerkea <113178110+frannerkea@users.noreply.github.com>
2023-02-06 13:35:35 +0000

    Done done

---
+++ fix-samples.sh
@@ -0,0 +1,12 @@
+cd samples/
+mv animals/countries  .
+mv countries/movies  .
+mv animals/Monkies.txt animals/Monkeys.txt
+echo Monkeys > animals/Monkeys.txt
+mv movies/Germany.txt countries/
+my countries/.apes.txt animals/
+cd -
+
+#cd movie
+#cat Sex.txt And.txt The.txt City.txt >> "Sex And The City".txt
+#rm Sex.txt And.txt The.txt City.txt
